41a18075c8f5c042f05c6d3feb549290ec6a027e
Ayoub Farhi <167337139+AyoubFarhi1@users.noreply.github.com>
2024-04-28 16:58:44 +0000

    hi

--- 0x09-python-everything_is_object/tasks_adv.py
+++ 0x09-python-everything_is_object/tasks_adv.py
@@ -3,22 +3,22 @@
 # Define the questions and their corresponding answers
 questions_answers = {
     "31": {
-        "103-line1.txt": "1",
-        "103-line2.txt": "1"
+        "103-line1.txt": "0",
+        "103-line2.txt": "0"
     },
     "32": {
         "104-line1.txt": "1",
         "104-line2.txt": "1",
-        "104-line3.txt": "No",
+        "104-line3.txt": "yes",
         "104-line4.txt": "Yes",
         "104-line5.txt": "1"
     },
     "33": {
-        "105-line1.txt": "99"
+        "105-line1.txt": "262"
     },
     "34": {
         "106-line1.txt": "1",
-        "106-line2.txt": "1",
+        "106-line2.txt": "0",
         "106-line3.txt": "No",
         "106-line4.txt": "Yes",
         "106-line5.txt": "1"
